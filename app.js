c08d3f6112e3c54c19f3ebb96ff32062d7276dd9
Milos Milinkov <milos300399@gmail.com>
2025-04-21 16:32:16 +0200

    Blog post app - init version

---
+++ app.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import expressEjsLayouts from 'express-ejs-layouts';
+import router from './server/routes/index.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+
+app.use(expressEjsLayouts);
+app.set('layout', './layout/main');
+app.set('view engine', 'ejs');
+
+app.use(router);
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}.`);
+});
