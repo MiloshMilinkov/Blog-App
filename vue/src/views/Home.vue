f58d7ee643cb3c2edef638201562491553a67986
Milos Milinkov <milos300399@gmail.com>
2025-05-04 16:14:18 +0200

    Feature: Added base Vite project

---
+++ vue/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>All Posts</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+          {{ post.title }}
+      </li>
+    </ul>
+</template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import api from '../api';
+  
+  const posts = ref([]);
+  
+  onMounted(async () => {
+    const res = await api.get('/posts');
+    posts.value = res.data;
+  });
+  </script>
+  
