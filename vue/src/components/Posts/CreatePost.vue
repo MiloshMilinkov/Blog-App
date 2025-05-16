ea48d470f4a6f7cbd04f405add3d666358ec17dd
Milos Milinkov <milos300399@gmail.com>
2025-05-16 17:27:48 +0200

    Client: full tailwind style for Create Post tab

--- vue/src/components/Posts/CreatePost.vue
+++ vue/src/components/Posts/CreatePost.vue
@@ -1,31 +1,40 @@
 <template>
   <BaseCard>
-    <div v-if="errorMessages.length" class="mb-4">
+    <div v-if="errorMessages.length" class="mb-4 text-red-600">
       <p v-for="(msg, i) in errorMessages" :key="i">
         {{ msg }}
       </p>
     </div>
-    <form @submit.prevent="updatePost">
+    <form @submit.prevent="updatePost" class="flex flex-col gap-5">
       <header>
-        <h2 class="text-3xl text-(--color-text) font-bold">Create a Post</h2>
+        <h2 class="text-3xl text-[--color-text] font-bold">Create a Post</h2>
       </header>
-      <div class="title_container font-bold">
-        <label for="title" class="text-(--color-text)">Title</label>
-        <input type="text" class="bg-green-100 border-2 border-(--color-border) rounded-lg">
+
+      <div class="font-bold flex flex-col gap-1.5">
+        <label for="title" class="text-[--color-text] font-semibold">Title</label>
+        <input
+          type="text"
+          v-model="form.title"
+          class="bg-green-100 border-2 border-[--color-border] rounded-lg p-3 text-base transition-colors focus:outline-none"
+        />
       </div>
 
-      <div class="body_container">
-        <label for="body" class="text-(--color-text) font-bold">Body</label>
-        <textarea class="bg-green-100 border-2 border-(--color-border) rounded-lg"/>
+      <div class="flex flex-col gap-1.5">
+        <label for="body" class="text-[--color-text] font-semibold">Body</label>
+        <textarea
+          v-model="form.body"
+          class="bg-green-100 border-2 border-[--color-border] rounded-lg p-3 text-base transition-colors focus:outline-none"
+        />
       </div>
 
-      <div >
+      <div class="flex flex-row gap-1.5">
         <BaseButton type="button" @click="goBack">Cancel</BaseButton>
         <BaseButton type="submit">Save Changes</BaseButton>
       </div>
     </form>
   </BaseCard>
 </template>
+
   
 <script setup>
   import { ref, reactive, onMounted  } from 'vue';
@@ -50,43 +59,4 @@ function goBack() {
   router.back(); 
 }
 </script>
-  
-<style scoped>
-@import "tailwindcss";
-form {
-  display: flex;
-  flex-direction: column;
-  gap: 1.5rem;
-}
-
-.title_container,
-.body_container,
-.mb-4 {
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-}
-
-input,
-textarea {
-  padding: 0.75rem;
-  font-size: 1rem;
-  transition: background-color 0.2s;
-}
-
-input:focus,
-textarea:focus {
-  outline: none;
-}
-
-label {
-  font-weight: 600;
-}
-
-form > div:last-child {
-  display: flex;
-  justify-content: flex-end;
-  gap: 1rem;
-}
-</style>
   
